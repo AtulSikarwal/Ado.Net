4ff5cdc7e7bac0b10477f63a891605293adc1959
AtulSikarwal <atulsikarwal5.com>
2022-07-23 16:03:36 +0530

    The Code for Update the Table Data

---
+++ UpdateTable/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace AddressBookDbConsole
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            EmployeeRepositoey res=new EmployeeRepositoey();
+
+            EmployeeModel model=new EmployeeModel();
+            model.Name = "Atul";
+            model.EmployeeId = 27;
+            model.Salary = 3353;
+
+           model.Gender = "M";
+          model.Address = "NEB";
+            model.PhoneNumber = 998392291;
+          //  res.AddEmployee(model);
+              res.UpdateEmployee(model);
+            //  res.DeleteEmployee(model);
+          //  res.InsertIntoTwoTable(model);
+         //   res.InsertIntoTwoTablesWithTranactions();
+            res.GetAllEmployees();
+        }
+    }
+}
